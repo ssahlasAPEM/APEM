11d5eced2ea0aaeedca0f04f006602de3d639b31
apengstrom <andrew@curiousm.com>
2016-07-11 09:18:07 -0400

    Fix html error
    Signed-off-by: apengstrom <andrew@curiousm.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,23 +1,15 @@
 @extends('layouts.master')
 
-<form method="POST" action="/auth/login">
+<form class="form-signin" method="POST" action="/auth/login">
     {!! csrf_field() !!}
 
-    <div>
-        Email
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
-
-    <div>
-        Password
-        <input type="password" name="password" id="password">
-    </div>
-
-    <div>
-        <input type="checkbox" name="remember"> Remember Me
-    </div>
-
-    <div>
-        <button type="submit">Login</button>
-    </div>
+    <img alt="APEM LOGO" />
+    <h2 class="form-signin-heading">NAO LOGIN TITLE TEXT</h2>
+    <input type="email" name="email" class="form-control" placeholder="username" required="" autofocus="" value="{{ old('email') }}">
+    <br>
+    <input type="password" name="password" id="password" class="form-control" placeholder="password" required="">
+    <br>
+    <input type="checkbox" name="remember"> Remember Me
+    <br>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
 </form>
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,12 +13,12 @@
         {{-- !!HTML::style('//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css')!! --}}
 
         <!-- Icon Font -->
-        {!!HTML::style('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css')!!}
+        {!!Html::style('//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css')!!}
 
         <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-        {!!HTML::script('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js')!!}
+        {!!Html::script('https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js')!!}
         <!-- Bootstrap's JavaScript plugins) -->
-        {!!HTML::script('//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js')!!}
+        {!!Html::script('//maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js')!!}
 
         <!-- Custom CSS -->
     </head>
@@ -26,11 +26,14 @@
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
                 <!-- MAIN CONTENT -->
                 @yield('content')
                 <!-- /END MAIN CONTENT -->
             </div>
+
+            <footer>
+                APEM Home | Help
+            </footer>
         </div>
     </body>
 </html>
