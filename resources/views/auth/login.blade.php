bec1de3aca394c4e999828b2f0262626712d7b81
Ralica <ralica@curiousm.com>
2016-07-12 10:06:33 -0400

    bootstrap selectors to shape login view
    Signed-off-by: Ralica <ralica@curiousm.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,22 +3,22 @@
 @section('title', 'Login')
 
 @section('content')
-<div class="center-block">
+<div class="center-block row">
 
     {!! csrf_field() !!}
     {!! Auth::user() !!}
 
     {!! Html::image('/images/corp-logo.PNG', 'APEM LOGO' , array('class' => 'apem-logo center-block')) !!}
 
-    <div class="center-text">NAO LOGIN TITLE TEXT</div>
+    <div class="text-center">NAO LOGIN TITLE TEXT</div>
 
     {!! Html::ul($errors->all(), array('class'=>'errors')) !!}
 
-    {!! Form::open(array('url' => 'login','class'=>'form-signin')) !!}
+    {!! Form::open(array('url' => 'login','class'=>'center-block form-signin col-md-4 clearfix')) !!}
 
-    {!! Form::text('username', old('username'), array('class' => 'form-control center-text', 'placeholder'=>'Username')) !!}
-    {!! Form::password('password', array('class' => 'form-control center-text', 'placeholder'=>'Password')) !!}
+    {!! Form::text('username', old('username'), array('class' => 'form-control text-center', 'placeholder'=>'Username')) !!}
+    {!! Form::password('password', array('class' => 'form-control text-center', 'placeholder'=>'Password')) !!}
     {!! Form::submit('Login Btn' , array('class' => 'btn btn-default btn-block login-btn')) !!}
     {!! Form::close() !!}
-
+</div>
 @endsection
