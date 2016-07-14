3b6a6d54dff40d1b2001c3b16d4a3c70eeb4ba87
Ralica <ralica@curiousm.com>
2016-07-14 08:33:02 -0400

    Cors
    Signed-off-by: Ralica <ralica@curiousm.com>

--- public/index.php
+++ public/index.php
@@ -46,6 +46,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: X-Requested-With, Content-Type');
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
