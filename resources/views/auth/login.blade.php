43d71796f5fe29e86e65297ba286a294aa8564b9
Ralica <ralica@curiousm.com>
2016-07-25 10:58:06 -0400

    Styled login page
    Signed-off-by: Ralica <ralica@curiousm.com>

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,19 +8,17 @@
 {!! csrf_field() !!}
 {!! Auth::user() !!}
 
-<div class="row gray-border">
+<div class="row gray-border login-box">
   <div class="">
     {!! Html::image('assets/images/corp-logo.PNG', 'APEM LOGO' , array('class' => 'apem-logo center-block')) !!}
 
-
-
-    <div class="col-sm-12 top-border gray-bg">
+    <div class="col-sm-12 ">
       {!! Form::open(array('url' => 'login','class'=>'form-signin')) !!}
-        <div class="text-center gap5">NAO LOGIN TITLE TEXT</div>
-        <div class="form-group login-controlls gray-bg">
-          {!! Form::text('username', old('username'), array('class' => 'form-control text-center gap5 transparent-input', 'placeholder'=>'Username')) !!}
-          {!! Form::password('password', array('class' => 'form-control text-center transparent-input', 'placeholder'=>'Password')) !!}
-          {!! Form::submit('Login Btn' , array('class' => 'btn btn-default btn-block login-btn gap2')) !!}
+        <!-- <div class="text-center gap5">NAO LOGIN TITLE TEXT</div> -->
+        <div class="form-group login-controlls">
+          {!! Form::text('username', old('username'), array('class' => 'form-control user-input', 'placeholder'=>'Username')) !!}
+          {!! Form::password('password', array('class' => 'form-control gap1 pass-input', 'placeholder'=>'Password')) !!}
+          {!! Form::submit('Login' , array('class' => 'btn btn-default btn-block login-btn gap1')) !!}
         </div>
 
       {!! Form::close() !!}
