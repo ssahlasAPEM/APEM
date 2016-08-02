f5e0ab78e9d6f7b0aa1f77118dfbee43f8c3df8f
apengstrom <andrew@curiousm.com>
2016-08-02 12:10:53 -0400

    Test configurations
    Signed-off-by: apengstrom <andrew@curiousm.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -1,13 +1,24 @@
 <?php
 
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
 class TestCase extends Illuminate\Foundation\Testing\TestCase
 {
+    use DatabaseMigrations;
+    use DatabaseTransactions;
+
     /**
      * The base URL to use while testing the application.
      *
      * @var string
      */
     protected $baseUrl = 'http://localhost';
+
+    protected $headers = [
+        'CONTENT_TYPE' => 'application/vnd.api+json',
+        'HTTP_X-Requested-With' => 'XMLHttpRequest'
+    ];
 
     /**
      * Creates the application.
@@ -21,5 +32,22 @@ class TestCase extends Illuminate\Foundation\Testing\TestCase
         $app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
 
         return $app;
+    }
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        $credentials = [
+            'username' => 'TestHarness',
+            'password' => 'X9Z2gPd9',
+        ];
+
+        Auth::attempt($credentials, true);
+    }
+
+    public function tearDown()
+    {
+        parent::tearDown();
     }
 }
