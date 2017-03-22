307e80bada882b998b46e38e6c6121d9fafd9c8c
apengstrom <apengstrom@gmail.com>
2017-03-22 15:34:26 -0400

    dump the event class to see why it’s still interpreting as a string
    Signed-off-by: apengstrom <apengstrom@gmail.com>

--- app/Core/Opportunity/Model/Opportunity.php
+++ app/Core/Opportunity/Model/Opportunity.php
@@ -85,6 +85,7 @@ class Opportunity extends DomainEntity
         parent::__construct($dbId);
 
         $evtClass = Event::class;
+        var_dump($evtClass);
         $events = $evtClass->where('opportunity_id','=',$dbId)->get();
 
         foreach($events as $event) {
